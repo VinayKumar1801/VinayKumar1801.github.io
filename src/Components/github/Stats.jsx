bfbf618e2389c8bc3fc7e139490356650e52a287
VinayKumar1801 <107485549+VinayKumar1801@users.noreply.github.com>
2023-01-13 13:41:19 +0530

    add stats

--- src/Components/github/Stats.jsx
+++ src/Components/github/Stats.jsx
@@ -3,29 +3,41 @@ import React from 'react'
 
 const Stats = () => {
   return (
-    <Box  textAlign={"center"} w={"75%"} m={"auto"} >
+    <Box textAlign={"center"} w={"75%"} m={"auto"}>
       <h2 className="section__title">My Statistics</h2>
-      <Box m="auto" mt={"7"}>
-        <Link href="https://github.com/VinayKumar1801" isExternal>
+      <Box m={"auto"} w={"full"} mt="2rem">
+        <Link href="https://github.com/isky27" isExternal>
           <Image
             m={"auto"}
             w={{ lg: "60%", md: "70%", sm: "90%" }}
-            src="https://github-readme-streak-stats.herokuapp.com?user=VinayKumar1801"
+            src="https://github-readme-streak-stats.herokuapp.com?user=vinaykumar1801&theme=nord"
             alt="Github Statistics"
           />
         </Link>
       </Box>
-      <Flex w={"full"} m="auto" mt={"7"} justify={"space-around"} gap="2rem" flexDirection={{lg:"row",base:"column",sm:"column",md:"row"}}>
+
+      <Flex
+        w={"full"}
+        m="auto"
+        mt={"7"}
+        justify={"space-around"}
+        gap="2rem"
+        flexDirection={{ lg: "row", base: "column", sm: "column", md: "row" }}
+      >
         <Link href="https://github.com/VinayKumar1801" isExternal>
           <Image
-            align="center"
-            src="https://github-readme-stats.vercel.app/api/top-langs/?username=VinayKumar1801" //launguages
+            // align="center"
+            margin={"auto"}
+            display={{ lg: "inline", md: "block", sm: "block", base: "block" }}
+            src="http://github-profile-summary-cards.vercel.app/api/cards/stats?username=vinaykumar1801&theme=nord_dark" //launguages
           />
         </Link>
         <Link href="https://github.com/VinayKumar1801" isExternal>
           <Image
-            align="left"
-            src="https://github-readme-stats.vercel.app/api?username=VinayKumar1801&count_private=true&show_icons=true" //stats
+            // align="left"
+            margin={"auto"}
+            display={{ lg: "inline", md: "block", sm: "block", base: "block" }}
+            src="http://github-profile-summary-cards.vercel.app/api/cards/repos-per-language?username=vinaykumar1801&theme=nord_dark" //stats
           />
         </Link>
       </Flex>
