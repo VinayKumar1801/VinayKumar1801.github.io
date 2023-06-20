35c10ad43ab74bb012f56b22f036d118d4978de7
vinaykumar1303 <vinay@flamecloud.co.uk>
2023-06-20 07:45:30 +0530

    ui

--- src/Components/projects/Projects.jsx
+++ src/Components/projects/Projects.jsx
@@ -1,22 +1,4 @@
-// import React from 'react'
-// import Project1 from './Project1'
-// import Project2 from './Project2'
-// import Project3 from './Project3'
-// const Projects = () => {
-//   return (
-//     <section className="projects section" id="projects">
-//     <h2 className="section__title">Projects</h2>
-//     <span className="section__subtitle">The things I make</span>
-//     <Project1/>
-//     <Project2/>
-//     <Project3/>
-//     </section>
-    
-    
-//   )
-// }
 
-// export default Projects
 import React,{useEffect} from 'react'
 import {
   Box,
